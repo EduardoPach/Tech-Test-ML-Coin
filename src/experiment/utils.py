ff1ac174d3ec17ccc86f638a0f554a8370847c67
EduardoPach <eduardo.pach@hotmail.com>
2023-09-29 16:20:32 -0300

    Removed print

--- src/experiment/utils.py
+++ src/experiment/utils.py
@@ -82,7 +82,6 @@ def log_table(data: List[Dict[str, Any]]) -> None:
         prediction = d["prediction"]
         image = d["image"]
         subset = d["subset"]
-        print(f"{ground_truth.shape=}, {prediction.shape=}")
         img = wandb.Image(
             image,
             masks={
